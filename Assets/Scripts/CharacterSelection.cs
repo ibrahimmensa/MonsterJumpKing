325aab01245cc06f0375b7e45a4a4a3d0b2d61ab
ASAD11001 <asad11001work@gmail.com>
2022-10-03 00:28:25 +0500

    Character Selection

---
+++ Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+    public List<GameObject> CharactersList;
+    int currentCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindActiveCharacter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void NextCharacter()
+    {
+        DisbaleAll();
+        AssignCurrentCharacter(currentCharacter);
+        CharactersList[currentCharacter].SetActive(true);
+    }
+    public void PreviousCharacter()
+    {
+        DisbaleAll();
+        AssignCurrentCharacter(currentCharacter);
+        CharactersList[currentCharacter].SetActive(true);
+    }
+    public void DisbaleAll()
+    {
+        for (int i = 0; i < CharactersList.Count; i++)
+        {
+            CharactersList[i].SetActive(false);
+        }
+    }
+    public void AssignCurrentCharacter(int i)
+    {
+        currentCharacter = (i = 1) % CharactersList.Count;
+    }
+    public void FindActiveCharacter()
+    {
+        for (int i = 0; i < CharactersList.Count; i++)
+        {
+            if(CharactersList[i].activeSelf)
+            {
+                currentCharacter = i;
+            }
+        }
+    }
+}
